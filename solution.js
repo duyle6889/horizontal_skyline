6d4409557f997acb33009e7a9806f5b36e532d50
duyle <31949398+duyle6889@users.noreply.github.com>
2022-04-07 18:21:25 +0900

    Update solution.js

--- solution.js
+++ solution.js
@@ -2,7 +2,7 @@ function Solution(A)
 {
     let brushCount = 0;
     let prevHeight = 0;
-    for(int i = 0; i < A.length; i++)
+    for(let i = 0; i < A.length; i++)
     {
         if(A[i] > prevHeight) {
              brushCount = brushCount + (A[i] - prevHeight);
